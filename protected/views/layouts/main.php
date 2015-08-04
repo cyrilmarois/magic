4483946ab76f3898586d680084393ae0ee6a866a
cyrilmarois <cyrilmarois@gmail.com>
2015-08-04 15:46:32 +0200

    add main css

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -14,68 +14,68 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?php echo Yii::$app->language ?>">
-<head>
-    <meta charset="<?php echo Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php echo Html::csrfMetaTags() ?>
-    <title><?php echo Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
+    <head>
+        <meta charset="<?php echo Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php echo Html::csrfMetaTags() ?>
+        <title><?php echo Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
 
-<?php $this->beginBody() ?>
-    <div class="wrap">
-        <?php
-            NavBar::begin([
-                'brandLabel' => 'My Company',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
-                    Yii::$app->user->isGuest ?
-                        ['label' => 'Mon espace', 'url' => [Yii::$app->user->loginUrl]] :
-                        ['label' => 'Me déconnecter',
-                            'url' => ['/authenticate/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
-                ],
-            ]);
-            NavBar::end();
-        ?>
-
-        <div class="container">
+    <?php $this->beginBody() ?>
+        <div class="wrap">
             <?php
-                echo Breadcrumbs::widget([
-                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                NavBar::begin([
+                    'brandLabel' => 'My Company',
+                    'brandUrl' => Yii::$app->homeUrl,
+                    'options' => [
+                        'class' => 'navbar-inverse navbar-fixed-top',
+                    ],
                 ]);
-
-                if (Yii::$app->user->identity !== null) {
-                    echo Sidebar::widget();
-                    echo Html::beginTag('div', ['class' => 'main']);
-                }
-                    echo $content;
-
-                if (Yii::$app->user->identity !== null) {
-                    echo Html::endTag('div');
-                }
+                echo Nav::widget([
+                    'options' => ['class' => 'navbar-nav navbar-right'],
+                    'items' => [
+                        ['label' => 'Home', 'url' => ['/site/index']],
+                        ['label' => 'About', 'url' => ['/site/about']],
+                        ['label' => 'Contact', 'url' => ['/site/contact']],
+                        Yii::$app->user->isGuest ?
+                            ['label' => 'Mon espace', 'url' => [Yii::$app->user->loginUrl]] :
+                            ['label' => 'Me déconnecter',
+                                'url' => ['/authenticate/logout'],
+                                'linkOptions' => ['data-method' => 'post']],
+                    ],
+                ]);
+                NavBar::end();
             ?>
-        </div>
-    </div>
 
-    <footer class="footer">
-        <div class="container">
-            <p class="pull-left">&copy; My Company <?php echo date('Y') ?></p>
-            <p class="pull-right"><?php echo Yii::powered() ?></p>
-        </div>
-    </footer>
+            <div class="container">
+                <?php
+                    echo Breadcrumbs::widget([
+                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                    ]);
 
-<?php $this->endBody() ?>
-</body>
+                    if (Yii::$app->user->identity !== null) {
+                        echo Sidebar::widget();
+                        echo Html::beginTag('div', ['class' => 'main']);
+                    }
+                        echo $content;
+
+                    if (Yii::$app->user->identity !== null) {
+                        echo Html::endTag('div');
+                    }
+                ?>
+            </div>
+        </div>
+
+        <footer class="footer">
+            <div class="container">
+                <p class="pull-left">&copy; My Company <?php echo date('Y') ?></p>
+                <p class="pull-right"><?php echo Yii::powered() ?></p>
+            </div>
+        </footer>
+
+    <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
