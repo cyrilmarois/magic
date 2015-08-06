49839582ca4553d164120e0880ea9e6d7aa6d23d
cyrilmarois <cyrilmarois@gmail.com>
2015-08-06 14:56:53 +0200

    card search card functionality

---
+++ js/search.js
@@ -0,0 +1,41 @@
+function fetch(self) {
+    var val = self.val();
+    $.ajax({
+        type: 'post',
+        url: fetchCardUrl,
+        data: {formData: val}
+    })
+    .success(function(data) {
+        $('.dropdown-menu').children().remove();
+        if (data === '') {
+            var str = '<li>No results</li>'
+            $('.dropdown-menu').append(str);
+        } else {
+            var cards = []
+            $.each(data, function (ind, element) {
+                console.log(element.setIconUrl);
+                var card = '<li>' +
+                    '<a href="'+cardUrl+'&cardId=' + element.cardId + '">' + element.cardNameVO + '</a> --' +
+                    '<img src="'+ element.setIconUrl + '" alt="" width="40" height="30" />' +
+                    '</li>';
+                cards.push(card);
+            });
+            $('.dropdown-menu').append(cards);
+        }
+        $('.dropdown-menu').show();
+    });
+}
+
+
+
+$(document).ready(function() {
+    $('#search').on('keyup', function() {
+        fetch($(this));
+    });
+
+    $('body').on('click', function() {
+        if ($('.dropdown-menu').children().length > 0) {
+            $('.dropdown-menu').hide();
+        }
+    });
+});
