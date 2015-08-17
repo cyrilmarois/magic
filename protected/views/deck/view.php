fbfe1b4dd5028abafc2e1aead949a6c72e233169
cyrilmarois <cyrilmarois@gmail.com>
2015-08-18 00:39:27 +0200

    add ajax filter based on type and card cost in deck view

--- protected/views/deck/view.php
+++ protected/views/deck/view.php
@@ -6,10 +6,12 @@
  * Time: 19:33
  */
 
-use app\models\Card;
 use yii\helpers\Html;
+
+$this->registerJsFile(Yii::getAlias('@web/js/deck.js'), ['position' => \yii\web\View::POS_END, 'depends' => [\yii\web\JqueryAsset::className()]]);
 ?>
 <h1><?php echo $deck->deckName /*.'('./$totalCards.')'. $deckManas*/; ?></h1>
+<?php echo Html::activeHiddenInput($deck, 'deckId'); ?>
 Filtrer par : <?php echo Html::dropDownList('filter', 'types', ['types' => 'types', 'cost' => 'cost']); ?>
 
 <div class="deck">
