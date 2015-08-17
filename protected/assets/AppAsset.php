1d467aa2599aedea11fd5e1139422a85c8b8f778
cyrilmarois <cyrilmarois@gmail.com>
2015-08-18 01:07:51 +0200

    add facebook js

--- protected/assets/AppAsset.php
+++ protected/assets/AppAsset.php
@@ -24,6 +24,7 @@ class AppAsset extends AssetBundle
     public $js = [
         'js/search.js',
         'js/cards.js',
+        'js/facebook.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
